2fb6ccbfbc6eb4a1d81b7e45687bfc46fbe33f6d
Paulo H. <paulohtolotti@alumni.usp.br>
2025-04-05 19:17:18 -0300

    style: finalização da estilização

--- dama.php
+++ dama.php
@@ -20,26 +20,26 @@
         </nav>
     </header>
     <main>
-        <div class="dama">
             <h1>Dama</h1>
-            <?php
-            $data = json_decode(file_get_contents('./json_data/itens.json'), true);
-            foreach ($data['dama'] as $item) {
-                echo '<div class="game-wrap">';
-                echo '<h2>' . htmlspecialchars($item['nome']) . '</h2>';
-                if (is_array($item['imagem'])) {
-                    foreach ($item['imagem'] as $img) {
-                        echo '<img src="' . htmlspecialchars($img) . '"style="width: 300px; height: auto;" class="hero-img"' . '">';
+            <div class="game-grid">
+                <?php
+                $data = json_decode(file_get_contents('./json_data/itens.json'), true);
+                foreach ($data['dama'] as $item) {
+                    echo '<div class="game-wrap">';
+                    echo '<h2>' . htmlspecialchars($item['nome']) . '</h2>';
+                    echo '<p class="hero-text">' . htmlspecialchars($item['descricao']) . '</p>';
+                    if (is_array($item['imagem'])) {
+                        foreach ($item['imagem'] as $img) {
+                            echo '<img src="' . htmlspecialchars($img) . '"style="width: 300px; height: auto;" class="hero-img"' . '">';
+                        }
+                    } else {
+                        echo '<img src="' . htmlspecialchars($item['imagem']) . '"style="width: 300px; height: auto;" alt=""' . '">';
                     }
-                } else {
-                    echo '<img src="' . htmlspecialchars($item['imagem']) . '"style="width: 300px; height: auto;" alt=""' . '">';
+                    htmlspecialchars($item['nome']) . '">';
+                    echo '</div>';
                 }
-                htmlspecialchars($item['nome']) . '">';
-                echo '<p>' . htmlspecialchars($item['descricao']) . '</p>';
-                echo '</div>';
-            }
-            ?>
-        </div>
+                ?>
+            </div>
     </main>
     <footer>
         <p class="site-footer">Desenvolvido por: Artur Lombardi - Luis Paulo Andrade Silva - Matheus Batista Gonçalves - Paulo Henrique Tolotti </p>
